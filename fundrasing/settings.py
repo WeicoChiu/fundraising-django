4aed74528f021e9a6e4d5c5dae902cdebe814959
Weico <weico.chiu@gmail.com>
2021-11-23 23:10:33 +0800

    Add gunicorn dependency
    Prepare to deploy into GCP, add gunicorn for handle
    django app.
    
    Add ALLOW-HOST value for GCP IP.
    
    #20211123001

--- fundrasing/settings.py
+++ fundrasing/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-*fr-(0g@mz6onnt&n2r=^2rthr5c_sp&i^@rgug(^bv*#(#n@l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.81.165.130']
 
 
 # Application definition
