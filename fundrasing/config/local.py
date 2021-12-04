f58ec2934cbfa949f3eb175d220191760e342300
Weico <weico.chiu@gmail.com>
2021-12-04 22:03:37 +0800

    Separate to local and production
    Hide Django Secret Key in the supervisor by environment variable.
    
    #20211204001

---
+++ fundrasing/config/local.py
@@ -0,0 +1,5 @@
+from fundrasing.settings import *
+
+SECRET_KEY = 'development-secret-key'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
--- fundrasing/settings.py
+++ fundrasing/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*fr-(0g@mz6onnt&n2r=^2rthr5c_sp&i^@rgug(^bv*#(#n@l'
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-*fr-(0g@mz6onnt&n2r=^2rthr5c_sp&i^@rgug(^bv*#(#n@l'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.craking-enigma.com']
+# ALLOWED_HOSTS = ['127.0.0.1', '.craking-enigma.com']
 
 
 # Application definition
